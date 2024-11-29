1a86ae54bc8c3dcda8f913a4bf14cc09c10f40d7
YoEnte <anthony@roeder@ive.de>
2024-11-29 12:01:58 +0100

    finished showcase

--- logicShow.py
+++ logicShow.py
@@ -22,24 +22,35 @@ from socha.starter import Starter
 
 class Logic(IClientHandler):
     game_state: GameState
+    
+    def carrotsOther(self) -> int:
+        return self.game_state.clone_other_player().carrots
 
     # this method is called every time the server is requesting a new move
     # this method should always be implemented otherwise the client will be disqualified
     def calculate_move(self) -> Move:
 
         # Variable für alle possible Moves
+        poss = self.game_state.possible_moves()
 
         # Gib alle Felder
+        fields = self.game_state.board.track
 
         # Stats eigener Spieler
+        player = self.game_state.clone_current_player()
 
         # Karotten anderer Spieler in neuer Methode
-
+        carrots = self.carrotsOther()
+        
         # Ob ein spezieller Feldtyp in Reichweite ist
-
+        for p in poss:
+            if "Advance" in type(p.action):
+                if  self.game_state.board.get_field(player.position + p.action.distance) == Field.Salad:
+                    print(p)
+        
         # Noch eine Idee?
 
-        return random.choice(self.game_state.possible_moves())
+        return random.choice(poss)
 
     # this method is called every time the server has sent a new game state update
     # this method should be implemented to keep the game state up to date
